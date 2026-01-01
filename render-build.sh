1916f1c2a6130028e259d260a332d0b0a2002297
bhuvan <clgacc123@gmail.com>
2026-01-01 17:08:55 +0530

    Fix Deployment: Add wheel support

--- render-build.sh
+++ render-build.sh
@@ -9,5 +9,6 @@ npm run build --prefix frontend
 echo "Installing Backend Dependencies..."
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 pip install --upgrade pip
+pip install wheel setuptools
 pip install cmake
 pip install -r backend/requirements.txt --no-cache-dir
